80996890b6700c7d35f988502f51984aeaca3059
Isha Bali <ishabali@gmail.com>
2020-09-18 14:51:18 -0500

    Added fields to schema. Split route and controller to matchesYes and matchesNo

--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,32 @@ router
     .get(auth.authentication, usersController.findByName)    // get 1 user by name (:id1)   
     .put(auth.authentication, usersController.updateByName)      // update 1 user by userName (:id1)
 
-// update :id1 user's matchesYes array be adding :id2 (Name of matched user) in it
+// update :id1 user's matchesYes array be adding :id2 (Name of Yes matched user) in it
 //:id1 is user Name, :id2 user Name of match
 router
-.route("/usersmatches/:id1/:id2")    
+.route("/usersyesmatches/:id1/:id2")    
     .put(usersController.updateMatchesYesByName)   
- 
-    // get all info for all matches (for all users in matchesYes array) of user by id (:id1) 
+
+// update :id1 user's matchesNo array be adding :id2 (Name of No matched user) in it
+//:id1 is user Name, :id2 user Name of match
 router
-.route("/usersallmatches/:id1")            
-    .get(auth.authentication, usersController.getMatchesByName)    //id1 is user Name
+.route("/usersnomatches/:id1/:id2")    
+    .put(usersController.updateMatchesNoByName)     
+ 
+// get all info for all Yes matches (for all users in matchesYes array) of user by id (:id1) 
+router
+.route("/usersallyesmatches/:id1")            
+    .get(auth.authentication, usersController.getMatchesYesByName)    //id1 is user Name
 
+// get all info for all No matches (for all users in matchesNo array) of user by id (:id1) 
+router
+.route("/usersallnomatches/:id1")            
+    .get(auth.authentication, usersController.getMatchesNoByName)    //id1 is user Name
 
+module.exports = router;
+
+////////////////////////////////////////////////////////////////////////
 // Matches with "/api/posts/:id"
 // router
 //   .route("/:id")
 //   .delete(usersController.remove);
-
-module.exports = router;
